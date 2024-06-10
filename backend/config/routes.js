6918fa3c2d26081c910380a6184d56291877d00a
cleantho <cleantho@gmail.com>
2024-06-10 19:57:08 -0300

    API articles

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -12,11 +12,23 @@ module.exports = app => {
         .get(app.api.category.get)
 
     // Cuidado com ordem! Tem que vir antes de /categories/:id
-    app.route('/categories/tree')        
+    app.route('/categories/tree')
         .get(app.api.category.getTree)
 
     app.route('/categories/:id')
         .put(app.api.category.save)
         .get(app.api.category.getById)
         .delete(app.api.category.remove)
+
+    app.route('/articles')
+        .post(app.api.article.save)
+        .get(app.api.article.get)
+
+    app.route('/articles/:id')
+        .put(app.api.article.save)
+        .get(app.api.article.getById)
+        .delete(app.api.article.remove)
+
+    app.route('/categories/:id/articles')
+        .get(app.api.article.getByCategory)
 }
