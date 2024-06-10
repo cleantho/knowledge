b32cea6b79ec0ed15b9baeb16bb136663e4a6032
cleantho <cleantho@gmail.com>
2024-06-10 12:30:27 -0300

    API Categories

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -6,5 +6,17 @@ module.exports = app => {
     app.route('/users/:id')
         .put(app.api.user.save)
         .get(app.api.user.getById)
-       
+
+    app.route('/categories')
+        .post(app.api.category.save)
+        .get(app.api.category.get)
+
+    // Cuidado com ordem! Tem que vir antes de /categories/:id
+    app.route('/categories/tree')        
+        .get(app.api.category.getTree)
+
+    app.route('/categories/:id')
+        .put(app.api.category.save)
+        .get(app.api.category.getById)
+        .delete(app.api.category.remove)
 }
