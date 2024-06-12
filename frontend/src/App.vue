72884545a25360aa5272151b913d73f9e57e8b3b
cleantho <cleantho@gmail.com>
2024-06-12 11:53:33 -0300

    Initial structure

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,44 @@
 <template>
 	<div id="app">
-		<h1>Versão Inicial</h1>
+		<HeaderVue />
+		<MenuVue />		
+		<ContentVue />
+		<FooterVue />
 	</div>
 </template>
 
 <script>
+import HeaderVue from "@/components/template/Header"
+import MenuVue from "@/components/template/Menu"
+import ContentVue from "@/components/template/Content"
+import FooterVue from "@/components/template/Footer"
+
 export default {
 	name: "App",
+	components: { HeaderVue, MenuVue, ContentVue, FooterVue }
 }
 </script>
 
 <style>
+	* {
+		font-family: "Lato", sans-serif;
+	}
 
+	body {
+		margin: 0;
+	}
+
+	#app {
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+
+		height: 100vh;
+		display: grid;
+		grid-template-rows: 60px 1fr 40px;
+		grid-template-columns: 300px calc(100vw - 300px);
+		grid-template-areas:
+			"header header"
+			"menu content"
+			"menu footer";
+	}
 </style>
---
+++ frontend/src/components/template/Content.vue
@@ -0,0 +1,19 @@
+<template>
+    <section class="content">
+        
+    </section>
+</template>
+
+<script>
+export default {    
+    name: 'ContentVue'
+}
+</script>
+
+<style>
+    .content {
+        grid-area: content;        
+        background-color: #efefef;
+        padding: 20px;
+    }
+</style>
